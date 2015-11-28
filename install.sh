8848af7156ee97e4a619991010d6e188e60f857b
Kevin Vicrey <kevin.vicrey@nexu.org>
2015-11-28 11:06:54 -0500

    Added branchname in the prompt

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ rm ~/.vimrc ~/.zshrc ~/.tmux.conf
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
+
+echo 'source ~/.vickev/bashrc' >> ~/.bashrc
