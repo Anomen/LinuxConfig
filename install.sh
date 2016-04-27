a958edfa5a6f6f8556f4790017e5994254b7f805
Kevin Vicrey <kevin.vicrey@nexu.org>
2016-04-27 11:58:51 -0400

    Added gitconfig

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ rm ~/.vimrc ~/.zshrc ~/.tmux.conf
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.vickev/gitconfig ~/.gitconfig
 
 echo 'source ~/.vickev/bashrc' >> ~/.bashrc
