7130179469e74b7ce0562958253a37ece4c3ca8b
Kevin Vicrey <kevin.vicrey@nexu.org>
2016-11-20 17:09:27 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.themes/Anomen* ~/.themes/Orion ~/.themes/Ambiance* ~/.icons/Vibrancy* ~/.gitconfig
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.themes/Anomen* ~/.themes/Orion ~/.gitconfig
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.vickev/gitconfig ~/.gitconfig
 cp -r ~/.vickev/themes/* ~/.themes
+cp -r ~/.vickev/autostart/* ~/.config/autostart/
 
 sudo dpkg -i ~/.vickev/packages/*.deb
 sudo apt-get install -f
-
-# sound-application-switcher
-
 
 echo 'source ~/.vickev/bashrc' >> ~/.bashrc
