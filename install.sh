c6a087d473c23807252fb5d55d79ada28914f34b
Kevin Vicrey <kevin.vicrey@nexu.org>
2016-11-20 17:07:46 -0500

    Added deb packages

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.themes/Anomen* ~/.themes/Orion ~/.gitconfig
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.themes/Anomen* ~/.themes/Orion ~/.themes/Ambiance* ~/.icons/Vibrancy* ~/.gitconfig
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.vickev/gitconfig ~/.gitconfig
-ln -s ~/.vickev/themes/* ~/.themes
+cp -r ~/.vickev/themes/* ~/.themes
+
+sudo dpkg -i ~/.vickev/packages/*.deb
+sudo apt-get install -f
+
+# sound-application-switcher
+
 
 echo 'source ~/.vickev/bashrc' >> ~/.bashrc
