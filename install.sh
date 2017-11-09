f5f9f121cf6d98a70632c9e8b37e08ffdef53a87
Kevin Vicrey <kevin.vicrey@nexu.org>
2017-11-09 13:27:38 -0500

    Removed packages and compiz config

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.themes/Anomen* ~/.themes/Orion ~/.gitconfig ~/.config/compiz-1/compizconfig/*
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.vickev/gitconfig ~/.gitconfig
-ln -s ~/.vickev/autostart/* ~/.config/autostart/
-ln -s ~/.vickev/compiz/* ~/.config/compiz-1/compizconfig
-cp -r ~/.vickev/themes/* ~/.themes
-
-sudo dpkg -i ~/.vickev/packages/*.deb
-
-# Diodon
-sudo add-apt-repository ppa:diodon-team/stable
-sudo apt-get update
-
-sudo apt-get install -f diodon
+#ln -s ~/.vickev/compiz/* ~/.config/compiz-1/compizconfig
+#cp -r ~/.vickev/themes/* ~/.themes
 
 echo 'source ~/.vickev/bashrc' >> ~/.bashrc
