5bdeb19223e2f6e82c99595d42371afe19dfd226
Kevin Vicrey <kevin.vicrey@nexu.org>
2018-12-29 10:14:57 -0500

    Added inputrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.vickev/gitconfig ~/.gitconfig
+ln -s ~/.vickev/inputrc ~/.inputrc
 #ln -s ~/.vickev/compiz/* ~/.config/compiz-1/compizconfig
 #cp -r ~/.vickev/themes/* ~/.themes
 
