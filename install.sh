0af35c489be0f90da335c2f838ffe5717369e01f
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-03-02 13:16:19 -0500

    Add OMF

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish ~/.config/omf
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
 ln -s ~/.vickev/gitconfig ~/.gitconfig
 ln -s ~/.vickev/fish ~/.config/fish
+ln -s ~/.vickev/omf ~/.config/omf
 #ln -s ~/.vickev/compiz/* ~/.config/compiz-1/compizconfig
 #cp -r ~/.vickev/themes/* ~/.themes
 
