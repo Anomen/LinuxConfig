3941e3cf35500b64f17641850a899ab4424a76c7
Kevin Vicrey <kevin.vicrey@nexu.org>
2025-06-23 16:06:09 -0400

    Update installation script and sketchybar battery plugin

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish ~/.config/omf ~/.inputrc ~/.tmux ~/.config/yabai ~/.config/skhd
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish ~/.config/omf ~/.inputrc ~/.tmux ~/.config/yabai ~/.config/skhd ~/.config/sketchybar*
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
@@ -10,6 +10,8 @@ ln -s ~/.vickev/fish ~/.config/fish
 ln -s ~/.vickev/omf ~/.config/omf
 ln -s ~/.vickev/yabai ~/.config/yabai
 ln -s ~/.vickev/skhd ~/.config/skhd
+ln -s ~/.vickev/sketchybar ~/.config/sketchybar
+ln -s ~/.vickev/sketchybarrc ~/.config/sketchybarrc
 
 ## Install fish
 #if [ "$OSTYPE" = "linux-gnu" ] ; then
--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+REMAINING="$(pmset -g batt | grep -Eo '[0-9]+%.*' | awk '{print $3}')"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 if [ "$PERCENTAGE" = "" ]; then
@@ -8,13 +9,13 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="🔋"
   ;;
   [6-8][0-9]) ICON=""
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON=""
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="🪫"
   ;;
   *) ICON=""
 esac
@@ -23,6 +24,12 @@ if [[ "$CHARGING" != "" ]]; then
   ICON=""
 fi
 
+if [[ "$REMAINING" == "(no" ]] ; then
+  REMAINING=""
+else
+  REMAINING=" (${REMAINING})"
+fi
+
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%${REMAINING}"
