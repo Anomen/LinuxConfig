2f0d78a5f189b47768318e2e28dc0a5d0f523cec
Kevin Vicrey <kevin.vicrey@nexu.org>
2025-06-22 20:49:58 -0400

    Added sketchybar

---
+++ sketchybar/plugins/speaker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# OUTPUT_NAME=$(SwitchAudioSource -t output -c)
+# VOLUME=$(osascript -e 'output volume of (get volume settings)')
+
+# case $OUTPUT_NAME in
+# 'MacBook Pro Microphone')
+#     OUTPUT_NAME="Mic"
+#     ;;
+# 'USB PnP Audio Device')
+#     OUTPUT_NAME="USB"
+#     ;;
+# esac
+
+# ICON=󰕾
+# HIGHLIGH=off
+# if [ $VOLUME -eq 0 ]; then
+#     ICON=󰕾
+#     HIGHLIGH=on
+# fi
+
+# sketchybar --set $NAME label="$OUTPUT_NAME ($VOLUME%)" icon=$ICON icon.highlight=$HIGHLIGH
