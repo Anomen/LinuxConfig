0d872c6eddf965a817af9ea2edd29a90f556c314
Kevin Vicrey <kevin.vicrey@nexu.org>
2025-06-23 19:18:21 -0400

    feat(sketchybar): add app icon mapping and space highlighting

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+# This script ONLY handles updating app icons and padding.
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+# --- IMPORTANT: SET ABSOLUTE PATHS ---
+YABAI_PATH="/opt/homebrew/bin/yabai"
+JQ_PATH="/opt/homebrew/bin/jq"
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+args=()
+for space_idx in $($YABAI_PATH -m query --displays | $JQ_PATH -r '.[].spaces | .[]'); do
+  icon_strip=$($YABAI_PATH -m query --windows --space "$space_idx" | \
+               $JQ_PATH -r '.[] | select(."is-sticky" == false and ."is-floating" == false and .app != "Finder") | .app' | \
+               sort -u | \
+               while IFS= read -r app_name; do
+                 printf "%s " "$($HOME/.config/sketchybar/plugins/icon_map.sh "$app_name")"
+               done)
+  
+  # Trim trailing space
+  icon_strip="${icon_strip% }"
+  
+  # Set label and padding
+  if [[ -n "$icon_strip" ]]; then
+    args+=(--set "space.$space_idx" label="$icon_strip" label.padding_right=10)
+  else
+    args+=(--set "space.$space_idx" label="" label.padding_right=0)
+  fi
+done
+
+sketchybar -m "${args[@]}"
---
+++ sketchybar/plugins/space_change.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script ONLY handles the color highlighting of the active space.
+
+# --- IMPORTANT: SET ABSOLUTE PATHS ---
+YABAI_PATH="/opt/homebrew/bin/yabai"
+JQ_PATH="/opt/homebrew/bin/jq"
+
+# --- COLOR DEFINITIONS ---
+INACTIVE_SPACE_COLOR=0x22ffffff
+ACTIVE_SPACE_COLOR=0xddffffff
+INACTIVE_FONT_COLOR=0xffffffff # White
+ACTIVE_FONT_COLOR=0xff1e1e2e   # A dark color (Catppuccin Macchiato Base)
+
+# Get the index of the currently focused space
+active_space_idx=$($YABAI_PATH -m query --spaces --space | $JQ_PATH .index)
+
+args=()
+for space_idx in $($YABAI_PATH -m query --displays | $JQ_PATH -r '.[].spaces | .[]'); do
+  # Set background and font colors based on whether the space is active
+  if [[ "$space_idx" == "$active_space_idx" ]]; then
+    args+=(--set "space.$space_idx" background.color=$ACTIVE_SPACE_COLOR \
+                                   icon.color=$ACTIVE_FONT_COLOR \
+                                   label.color=$ACTIVE_FONT_COLOR)
+  else
+    args+=(--set "space.$space_idx" background.color=$INACTIVE_SPACE_COLOR \
+                                   icon.color=$INACTIVE_FONT_COLOR \
+                                   label.color=$INACTIVE_FONT_COLOR)
+  fi
+done
+
+sketchybar -m "${args[@]}"
