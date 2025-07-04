2695fd99a1f965cc1676546acf343314bda01480
Kevin Vicrey <kevin.vicrey@nexu.org>
2025-07-04 11:30:14 -0400

    Improved install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish ~/.config/omf ~/.inputrc ~/.tmux ~/.config/yabai ~/.config/skhd ~/.config/sketchybar*
+rm -rfv ~/.vimrc ~/.zshrc ~/.tmux.conf ~/.gitconfig ~/.config/fish ~/.config/omf ~/.inputrc ~/.tmux ~/.config/yabai ~/.config/skhd ~/.config/sketchybar* ~/.p10k.zsh
 ln -s ~/.vickev/vim/vimrc ~/.vimrc
 ln -s ~/.vickev/zsh/zshrc ~/.zshrc
 ln -s ~/.vickev/tmux/tmux.conf ~/.tmux.conf
@@ -12,6 +12,7 @@ ln -s ~/.vickev/yabai ~/.config/yabai
 ln -s ~/.vickev/skhd ~/.config/skhd
 ln -s ~/.vickev/sketchybar ~/.config/sketchybar
 ln -s ~/.vickev/sketchybarrc ~/.config/sketchybarrc
+ln -s ~/.vickev/p10k.zsh ~/.p10k.zsh
 
 ## Install fish
 #if [ "$OSTYPE" = "linux-gnu" ] ; then
@@ -22,3 +23,9 @@ ln -s ~/.vickev/sketchybarrc ~/.config/sketchybarrc
 #fi;
 
 echo 'source ~/.vickev/bashrc' >> ~/.bashrc
+
+
+# Install zsh
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+brew install powerlevel10k zsh-autosuggestions zsh-syntax-highlighting eza zoxide
